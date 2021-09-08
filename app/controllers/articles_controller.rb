c180a6028f05556941fc0c2887abe371beed63ad
es404020 <es404020@gmail.com>
2021-09-09 00:40:15 +0100

    updated

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,12 @@
 class ArticlesController < ApplicationController
 
+
+    def index
+
+        @articles = Article.all
+
+    end     
+
     def  new
         @article = Article.new
     end
@@ -18,12 +25,47 @@ class ArticlesController < ApplicationController
             
         
 
+    end  
+    
+    
+    def edit
+        @article = Article.find(params[:id])
+
     end    
 
+
+    def update 
+        @article = Article.find(params[:id])
+
+        if @article.update(article_params)
+            flash[:notice]=" Article updated"
+            redirect_to article_path(@article)
+
+        else 
+            render 'edit'
+
+
+        end    
+            
+
+    end    
 
     def show
 
         @article = Article.find(params[:id])
+
+    end    
+
+
+
+    def destroy
+
+        @article = Article.find(params[:id])
+        @article.destroy
+        flash[:notice]=" Article Deleted"
+        redirect_to articles_path
+         
+
 
     end    
 
